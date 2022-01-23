c292bc174b28ae0ec62995fb103325403dd08a59
Jyoti-Mishra-123 <mishrajyoto3693@gmail.com>
2022-01-23 11:03:23 +0530

    code changes

---
+++ src/main/java/com/example/demo/GitDemoProj1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDemoProj1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitDemoProj1Application.class, args);
+	}
+
+}
