8fa16c41f449f71abad75160b33d0e6221df1aca
Jyoti-Mishra-123 <mishrajyoto3693@gmail.com>
2022-01-23 19:46:53 +0530

    changes-1

--- src/main/java/com/example/demo/GitDemoProj1Application.java
+++ src/main/java/com/example/demo/GitDemoProj1Application.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GitDemoProj1Application {
+String a="home";
+Integer b=7;
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitDemoProj1Application.class, args);
