56f44a00c2b559c832864a1a9ffb8a04e725f75e
Jyoti-Mishra-123 <mishrajyoto3693@gmail.com>
2022-01-23 12:57:03 +0530

    emp changes

--- src/main/java/com/example/demo/bean/Employee.java
+++ src/main/java/com/example/demo/bean/Employee.java
@@ -6,6 +6,33 @@ public class Employee {
 	private Integer eid;
 	private String ename;
 	private String addrs;
+	private String desg;
+	public Integer getEid() {
+		return eid;
+	}
+	public void setEid(Integer eid) {
+		this.eid = eid;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public String getAddrs() {
+		return addrs;
+	}
+	public void setAddrs(String addrs) {
+		this.addrs = addrs;
+	}
+	public String getDesg() {
+		return desg;
+	}
+	public void setDesg(String desg) {
+		this.desg = desg;
+	}
+	
+	
 	
 	
 	
