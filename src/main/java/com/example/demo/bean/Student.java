a92be90fe7f8b6aead0d9859818f1e601032dfcf
Jyoti-Mishra-123 <mishrajyoto3693@gmail.com>
2022-01-23 11:25:50 +0530

    task-2 commit

--- src/main/java/com/example/demo/bean/Student.java
+++ src/main/java/com/example/demo/bean/Student.java
@@ -1,5 +1,23 @@
 package com.example.demo.bean;
 
 public class Student {
+	//Task-2
+	
+	private Integer sid;
+	private String sname;
+	public Integer getSid() {
+		return sid;
+	}
+	public void setSid(Integer sid) {
+		this.sid = sid;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	
+	
 
 }
